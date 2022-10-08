12dedf5ef9a9809bd431a4dbc9536db75b6611cf
Omar Salazar R <fs.f4system@gmail.com>
2022-10-08 15:33:27 -0500

    PROYECTOFINAL - PFSalazar

--- src/app/students/components/edit-dialog/edit-dialog.component.ts
+++ src/app/students/components/edit-dialog/edit-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Student } from 'src/app/models/student';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-edit-dialog',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-dialog.component.scss']
 })
 export class EditDialogComponent implements OnInit {
+  fb: FormGroup;
 
-  constructor() { }
+  constructor(
+    private studentsService: StudentsService,
+    private dialogRef: MatDialogRef<EditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private student: Student
+  ) {
+    this.fb = new FormGroup({
+      studentName: new FormControl(student.studentName),
+      studentLastName: new FormControl(student.studentLastName),
+      age: new FormControl(student.age),
+      gender: new FormControl(student.gender),
+      cellPhoneNumber: new FormControl(student.cellPhoneNumber),
+      email: new FormControl(student.email),
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  editStudent() {
+    const c: Student = {
+      id: this.student.id,
+      studentName: this.fb.value.studentName,
+      studentLastName: this.fb.value.studentLastName,
+      age: this.fb.value.age,
+      gender: this.fb.value.gender,
+      cellPhoneNumber: this.fb.value.cellPhoneNumber,
+      email: this.fb.value.email,
+    }
+    this.studentsService.editStudent(c).subscribe((student: Student) => {
+      this.dialogRef.close(student);
+    })
+  }
 }
--- src/app/students/components/new-dialog/new-dialog.component.ts
+++ src/app/students/components/new-dialog/new-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-new-dialog',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-dialog.component.scss']
 })
 export class NewDialogComponent implements OnInit {
+  fb: FormGroup;
 
-  constructor() { }
+  constructor(
+    private studentsService: StudentsService,
+    private router: Router
+  ) {
+    this.fb = new FormGroup({
+      studentName: new FormControl(),
+      studentLastName: new FormControl(),
+      age: new FormControl(),
+      gender: new FormControl(),
+      cellPhoneNumber: new FormControl(),
+      email: new FormControl()
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  addStudent() {
+    const c: Student = {
+      id: '',
+      studentName: this.fb.value.studentName,
+      studentLastName: this.fb.value.studentLastName,
+      age: this.fb.value.age,
+      gender: this.fb.value.gender,
+      cellPhoneNumber: this.fb.value.cellPhoneNumber,
+      email: this.fb.value.email
+    }
+    this.studentsService.newStudent(c).subscribe((student: Student) => {
+      alert(`${student.id} - ${student.studentName} ${student.studentLastName} agregado correctamente.`);
+      this.router.navigate(['students/list']);
+    });
+  }
 }
